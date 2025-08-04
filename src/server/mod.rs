d2a0688e97b4806ce8e5fb4011b2fb0e1dcbed88
Stanley Masinde <stanmasinde@gmail.com>
2025-08-04 18:33:27 +0300

    proper error handling for the TCP listener

--- src/server/mod.rs
+++ src/server/mod.rs
@@ -1,7 +1,7 @@
 pub mod routes;
 use axum::Router;
 use reqwest::Client;
-use std::env::args;
+use std::{env::args, io::ErrorKind, process::exit};
 
 #[derive(Clone)]
 struct AppState {
@@ -16,11 +16,24 @@ pub async fn run() {
 
     let app = Router::new().merge(routes::routes());
 
-    let listener = tokio::net::TcpListener::bind(address)
+    let listener = match tokio::net::TcpListener::bind(address)
         .await
-        .inspect(|t| {
-            println!("Server started on http://{}", t.local_addr().unwrap())
-        })
-        .unwrap();
-    axum::serve(listener, app).await.unwrap();
+        .inspect(|t| println!("Server started on http://{}", t.local_addr().unwrap()))
+    {
+        Ok(l) => l,
+        Err(error) => {
+            if error.kind() == ErrorKind::PermissionDenied {
+                eprintln!("You don't have persmission to port {port}.")
+            } else if error.kind() == ErrorKind::AddrInUse {
+                eprintln!("Port {port} is already in use.")
+            } else {
+                eprintln!("Could not start the server {error}")
+            }
+
+            exit(1)
+        }
+    };
+    axum::serve(listener, app)
+        .await
+        .expect("Failed to start Axum.");
 }
